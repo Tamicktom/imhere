d459620be54e8efb9c39826b83a85065e90359b7
Tamicktom <rickpokemon38@gmail.com>
2023-12-08 20:47:05 -0300

    saving

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 //* Libraries imports
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 //* Local imports
 import { styles } from './styles';
@@ -9,7 +9,14 @@ export default function Home() {
   return (
     <View style={styles.container}>
       <StatusBar translucent style='light' />
-      <Text style={styles.label}>Olá mundo!</Text>
+      <Text style={styles.eventName}>Nome do envento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Nome do participante'
+        placeholderTextColor="#6b6b6b"
+        keyboardType='default'
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,13 +4,14 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#131016',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131016",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  label: {
-    color: 'rgb(220,220,220)',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
+  eventName: {},
+  eventDate: {},
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+  },
 });
