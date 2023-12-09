f2103d11d52ff450359ef30498c302750a4e99fe
Tamicktom <rickpokemon38@gmail.com>
2023-12-08 21:59:45 -0300

    add animations

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -14,11 +14,10 @@ export default function Participant(props: Props) {
   //when the component is rendered, do a bounce animation
   //to make it more fun
 
-  //create a new animated value
+  //create a new animated values
   const bounce = useRef(new Animated.Value(0)).current;
-
-  //create a new animated value
   const opacity = useRef(new Animated.Value(0)).current;
+  const height = useRef(new Animated.Value(0)).current;
 
   //when the component is rendered, do a bounce animation
   //to make it more fun
@@ -26,19 +25,47 @@ export default function Participant(props: Props) {
     Animated.parallel([
       Animated.spring(bounce, {
         toValue: 1,
-        useNativeDriver: true,
+        useNativeDriver: false,
       }),
       Animated.timing(opacity, {
         toValue: 1,
         duration: 500,
-        useNativeDriver: true,
+        useNativeDriver: false,
+      }),
+      Animated.spring(height, {
+        toValue: 56,
+        useNativeDriver: false,
       })
     ]).start();
   }, []);
 
+  //before the component is removed from the screen, do a fade out animation
+  const handleRemove = () => {
+    Animated.parallel([
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: false,
+      }),
+      Animated.spring(bounce, {
+        toValue: 0,
+        useNativeDriver: false,
+      }),
+      Animated.timing(height, {
+        toValue: 0,
+        duration: 250,
+        useNativeDriver: false,
+      })
+    ]).start();
+
+    //after the animation is done, call the onRemove function
+    setTimeout(() => props.onRemove(props.id), 500);
+  }
+
   return (
     <Animated.View style={[styles.container, {
       opacity: opacity,
+      height: height,
       transform: [
         {
           scale: bounce,
@@ -50,7 +77,7 @@ export default function Participant(props: Props) {
       <TouchableOpacity
         style={styles.button}
         activeOpacity={0.7}
-        onPress={() => props.onRemove(props.id)}
+        onPress={handleRemove}
       >
         <Text style={styles.buttonText}>-</Text>
       </TouchableOpacity>
